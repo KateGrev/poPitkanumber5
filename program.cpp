8969ffe20bb5bdb0ad1c9aad9fa8966fd16e109b
KateGrev <168267900+KateGrev@users.noreply.github.com>
2024-05-16 17:58:44 +0500

    Create program.cpp

---
+++ program.cpp
@@ -0,0 +1,11 @@
+#include "graphics.h"
+#include "picture.hpp"
+int main()
+{ initwindow(800,600);
+ home(); // äîì
+ sun(); // ñîëíöå
+ man(); // ÷åëîâåê
+ fence(); // çàáîð
+ getch();
+ closegraph();
+}
